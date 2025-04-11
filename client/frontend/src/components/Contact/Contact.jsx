ec90f7e19ee7c252078b925f3d006479825f421b
Vedant Gadge <vedant.gadgegsis@gmail.com>
2025-04-12 00:59:25 +0530

    Add GetStarted and Footer components; enhance Contact and Residencies styles

--- client/frontend/src/components/Contact/Contact.jsx
+++ client/frontend/src/components/Contact/Contact.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./Contact.css";
-import {MdCall} from 'react-icons/md'
-import {BsFillChatDotsFill} from 'react-icons/bs'
-import {HiChatBubbleBottomCenter} from 'react-icons/hi2'
-
+import { MdCall, MdChat } from "react-icons/md";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi2";
 
 const Contact = () => {
   return (
-    <section className="c-wrapper">
+    <div id='contact-us' className="c-wrapper">
       <div className="paddings innerWidth flexCenter c-container">
         {/* left side */}
         <div className="flexColStart c-left">
@@ -19,32 +18,75 @@ const Contact = () => {
             We believe a good place to live can make your life better.
           </span>
 
-            <div className="flexColStart contactChannels">
-
-                <div className="flexColStart row">
-                    <div className="flexColCenter mode">
-                    <div className="flexStart">
-                        <div className="flexCenter icon">
-                            <MdCall size={25} />
-                        </div>
-                        <div className="flexColStart detail">
-                            <span>Call</span>
-                            <span>021 123 145 14</span>
-                        </div>
-                    </div>
-                    </div>
+          <div className="flexColStart contactModes">
+            {/* first row */}
+            <div className="flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <MdCall size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Call</span>
+                    <span className="secondaryText">021 123 145 14</span>
+                  </div>
                 </div>
+                <div className="flexCenter button">Call now</div>
+              </div>
+
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Chat</span>
+                    <span className="secondaryText">021 123 145 14</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Chat now</div>
+              </div>
             </div>
 
+            {/* second row */}
+            <div className="flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Video Call</span>
+                    <span className="secondaryText">021 123 145 14</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Video Call now</div>
+              </div>
 
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <HiChatBubbleBottomCenter size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Message</span>
+                    <span className="secondaryText">021 123 145 14</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Message now</div>
+              </div>
+            </div>
+          </div>
         </div>
 
         {/* right side */}
-        <div className="c-right">
-          <img src="./contact.jpg" alt="" />
+        <div className="flexEnd c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" />
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
---
+++ client/frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <section className="f-wrapper">
+      <div className="paddings innerWidth flexCenter f-container">
+        <div className="flexColStart f-left">
+          <img src="./logo2.png" alt="" width={120} />
+
+          <span className="secondaryText">
+            Making your dream home a reality. <br />
+            Experience luxury living at its finest.
+          </span>
+        </div>
+
+        <div className="flexColStart f-right">
+          <span className="primaryText">Information</span>
+          <span className="secondaryText">145 New York, FL 4571, USA</span>
+
+          <div className="flexCenter f-menu">
+            <span>Properties</span>
+            <span>Services</span>
+            <span>Product</span>
+            <span>About Us</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
