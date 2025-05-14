3045c981c60f2a0fd263f4599d3ade4e3b37d2e5
Vedant Gadge <vedant.gadgegsis@gmail.com>
2025-05-14 13:13:59 +0530

    auth0 updated

--- client/frontend/src/main.jsx
+++ client/frontend/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     domain="dev-4h5pg7pbqxsolxzw.us.auth0.com"
     clientId="XnpwJf9y3DesIcfsNlohu8jZYJnFupN7"
     authorizationParams={{
-      redirect_uri: "http://localhost:5173",
+      redirect_uri: "https://real-estate-mern-vg.vercel.app",
     }}
     audience="http://localhost:8000"
     scope="openid profile email"
